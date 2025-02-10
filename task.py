59127f6f01bfe5bc51f92685cf92d1c0678a9f04
Munoz <mimunoz3@cps.k12.il.us>
2025-02-10 09:17:34 -0600

    changes

--- task.py
+++ task.py
@@ -7,33 +7,34 @@ def travel_destination(destination_options):
         }
 
     if destination_options in destinations: 
-        for line in destinations[destination_options]:
-            print(line)
+        for place in destinations[destination_options]:
+            print(place)
     else:
-        print("Sorry, I don't have any destination for that type of vacation.")
+  
+     print("Sorry, I don't have any destination for that type of vacation.")
 
 def main():
-    destinations = input("Do you want travel destination suggestions? yes/no").strip().lower()
+    destination = input("Do you want travel destination suggestions? yes/no").strip().lower()
 
-    if destinations == "no":
+    if destination == "no":
         print("Okay, maybe next time!")
         return 
     
-    while destinations == "yes":
+    while destination == "yes":
         print("Great!")
         question = input("Do you want a Beach, International, Camping, or City destination?").strip().lower()
 
-        travel_destination(destinations)
-
-        destinations = input("Do you want to explore other destinations? yes/not now").strip().lower()
-
-    if destinations == "not now":
+        travel_destination(question)
+        
+        destination = input("Do you want to explore other destinations? yes/not now").strip().lower()
+        
+    if destination == "not now":
         try:
             rate = int(input("Please rate our travel ideas 1-10"))
             final_score = rate * 10
-            print(f(final_score))
+            print(f"{final_score} percent satisfaction rate")
         except ValueError:
-            print("Invalit rating. Please enter a number between 1 and 10.")
+            print("Invalid rating. Please enter a number between 1 and 10.")
 
         friend = input("Would you recommend this to a friend? (yes/no/maybe):").strip().lower()
         if friend == "yes" or friend == "maybe":
